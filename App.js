488113a7c4f6bc07d41c6c14f8c45ce8f283d630
Karim Saadaoui <karim.saadaoui@medtech.tn>
2024-09-14 23:30:33 +0100

    homee + para+ daily needs

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
+import { StyleSheet } from "react-native";
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/Home';
+import MedicalAssistance from './screens/Paramedical';
+import DailyNeeds from "./screens/DailyNeeds";
+import Loisir from "./screens/Loisir";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ title: 'Home Screen' }}
+        />
+        <Stack.Screen 
+          name="MedicalAssistance" 
+          component={MedicalAssistance} 
+          options={{ title: 'Medical Assistance' }} 
+        />
+          <Stack.Screen 
+          name="DailyNeeds" 
+          component={DailyNeeds} 
+          options={{ title: 'Daily Needs' }} 
+        />
+          <Stack.Screen 
+          name="Loisir" 
+          component={Loisir} 
+          options={{ title: 'Entertainment' }} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#EAF2F8",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#EAF2F8",
+    justifyContent: "center",
   },
 });
